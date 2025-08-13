8d07b152310f2f57fb1988f6cee86cdc7693d78c
nearata <nearata@protonmail.com>
2025-08-13 20:21:45 +0200

    ci: add phpstan

--- extend.php
+++ extend.php
@@ -5,13 +5,13 @@ namespace Nearata\Boilerplate;
 use Flarum\Extend;
 
 return [
-	(new Extend\Frontend('forum'))
-		->js(__DIR__ . '/js/dist/forum.js')
-		->css(__DIR__ . '/less/forum.less'),
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
 
     (new Extend\Frontend('admin'))
-		->js(__DIR__ . '/js/dist/admin.js')
-		->css(__DIR__ . '/less/admin.less'),
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/less/admin.less'),
 
-    new Extend\Locales(__DIR__.'/locale')
+    new Extend\Locales(__DIR__.'/locale'),
 ];
