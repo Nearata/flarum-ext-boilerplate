2b852a4f6a823234517de5e7e3f542769bcfd5a7
nearata <nearata@protonmail.com>
2025-08-15 21:58:36 +0200

    ci: trigger

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,3 +1,3 @@
 import app from "flarum/forum/app";
 
-app.initializers.add("nearata-boilerplate", () => {function biome() {}});
+app.initializers.add("nearata-boilerplate", () => {function biome1() {}});
